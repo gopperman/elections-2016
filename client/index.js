003299a0d053c24b03ef0a20afd2e7431a797280
Gabriel Florit <gabrielflorit@gmail.com>
2016-08-04 18:41:45 -0400

    Add App

--- client/index.js
+++ client/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
+import App from './../common/components/App.js'
 
 // eslint-disable-next-line no-unused-vars
 import css from './../common/styles/config.styl'
 
 const rootElement = document.getElementById('root')
 
-render(<p>client</p>, rootElement)
+render(<App />, rootElement)
