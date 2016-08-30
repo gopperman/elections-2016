50200d75077fbeba9de8a405f49bb00a6f5cacb1
Gabriel Florit <gabrielflorit@gmail.com>
2016-08-30 12:55:30 -0400

    Fit mass map correctly

--- common/components/MassMap.js
+++ common/components/MassMap.js
@@ -16,20 +16,33 @@ class MassMap extends Component {
 		const race = (races && races[0]) || {}
 		console.log(race)
 
-		const towns =
-			topojson.feature(TOWNS, TOWNS.objects.TOWNS)
+		// convert TOWNS to a GeoJSON object (via topojson)
+		const towns = topojson.feature(TOWNS, TOWNS.objects.TOWNS)
 
-		const path = geoPath()
-		.projection(geoConicConformal()
+		// setup a MA-centric projection
+		const projection = geoConicConformal()
 			.parallels([41 + (43 / 60), 42 + (41 / 60)])
 			.rotate([71 + (30 / 60), -41])
-			.fitSize([400, 200], towns))
 
-		const d = path(towns)
+		// create the path
+		const path = geoPath().projection(projection)
+
+		// find MA bounds and aspect ratio for this projection
+		const b = path.bounds(towns)
+		const aspect = (b[1][0] - b[0][0]) / (b[1][1] - b[0][1])
+
+		// create width and heights, using 100 as width reference
+		const width = 100
+		const height = Math.round(width / aspect)
+
+		// fit this projection to the newly-calculated aspect ratio
+		projection.fitSize([width, height], towns)
 
 		return (
-			<svg className='MassMap'>
-				<path d={d} />
+			<svg
+				className='MassMap' viewBox={`0 0 ${width} ${height}`}
+				preserveAspectRatio='xMidYMax meet'>
+				<path d={path(towns)} />
 			</svg>
 		)
 
