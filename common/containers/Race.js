5529f3a5784167003acb7182f82e5b5d129168df
Greg Opperman <gopperman@gmail.com>
2016-08-31 14:54:39 -0400

    Copied presidential page for race page stub

---
+++ common/containers/Race.js
@@ -0,0 +1,94 @@
+import React, { Component, PropTypes } from 'react'
+import { provideHooks } from 'redial'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as actions from './../actions/actionCreators.js'
+import Timer from './../components/Timer.js'
+import ElectoralCollegeBar from './../components/ElectoralCollegeBar.js'
+import ElectoralCollegeMap from './../components/ElectoralCollegeMap.js'
+import MassMap from './../components/MassMap.js'
+
+const hooks = {
+	fetch: ({ dispatch }) =>
+		dispatch(actions.fetchResults({ url: 'president' })),
+}
+
+const mapDispatchToProps = (dispatch) => ({
+	dispatch,
+	actions: bindActionCreators(actions, dispatch),
+})
+
+@provideHooks(hooks)
+@connect(s => s, mapDispatchToProps)
+class Race extends Component {
+
+	static propTypes = {
+		actions: PropTypes.object.isRequired,
+		timer: PropTypes.object.isRequired,
+		results: PropTypes.object.isRequired,
+		dispatch: PropTypes.func.isRequired,
+	}
+
+	// when component is initially mounted onto DOM, fire its 'fetch' hook
+	componentDidMount = () => {
+		this.fetchData()
+	}
+
+	componentDidUpdate = (prevProps) => {
+
+		const { props } = this
+		const { startTimer, cancelTimer } = props.actions
+
+		// did we stop fetching?
+		if (prevProps.results.isFetching && !props.results.isFetching) {
+
+			// TODO: add data completeness check
+			++this.count
+			console.log(this.count)
+
+			// is the race over?
+			if (this.count > 0) {
+				cancelTimer()
+			} else {
+				startTimer()
+			}
+
+		}
+
+	}
+
+	fetchData = () => {
+		hooks.fetch({ dispatch: this.props.dispatch })
+	}
+
+	count = 0
+
+	render() {
+
+		const { props, fetchData } = this
+		const { timer, results } = props
+		const { stopTimer } = props.actions
+
+		const timerProps = {
+			...timer,
+			callback: () => {
+				stopTimer()
+				fetchData()
+			},
+		}
+
+		return (
+			<div className='Race'>
+				<h1>Race</h1>
+				<Timer {...timerProps} />
+				<MassMap data={results.data['president-ma-towns']} />
+				<ElectoralCollegeBar data={results.data['president-us']} />
+				<ElectoralCollegeMap data={results.data['president-us-states']} />
+			</div>
+		)
+
+	}
+
+}
+
+export default Race
--- common/routes.js
+++ common/routes.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Route } from 'react-router'
 import App from './components/App.js'
 import President from './containers/President.js'
+import Race from './containers/Race.js'
 
 export default (
 	<Route path='/' component={App}>
 		<Route path='president' component={President} />
+		<Route path='race' component={Race} />
 	</Route>
 )
