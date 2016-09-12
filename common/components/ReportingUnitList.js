45ba5a5164b17b899032abbda64261900131eb59
Greg Opperman <gopperman@gmail.com>
2016-09-12 15:23:25 -0400

    No returns

--- common/components/ReportingUnitList.js
+++ common/components/ReportingUnitList.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react'
-import { getTopCandidate } from './../utils/presidentialStateTable.js'
 
 // eslint-disable-next-line react/prefer-stateless-function
 class ReportingUnitList extends Component {
@@ -10,29 +9,26 @@ class ReportingUnitList extends Component {
 
 	render() {
 		const reportingUnits = this.props.data.reportingUnits
-		console.log(reportingUnits)
 		return (
 			<div className='ReportingUnitList'>
 				<h1>Town by Town Results</h1>
 				<ul>
-					{ reportingUnits.map( ru => {
-						return (
+					{ reportingUnits.map( ru => (
 							<li key={ru.reportingunitID}>
 								<h2>{ru.reportingunitName}</h2>
 								<p>Precincts Reporting: { ru.precinctsReporting } ({ ru.precinctsReportingPct}%)</p>
 								<ul>
-									{ ru.candidates.map( c => {
-										return (
+									{ ru.candidates.map( c => (
 											<li key={c.polID}>
 												<p>{c.first} {c.last} ({c.party})</p>
 												<p>Votes: {c.voteCount} {c.winner}</p>
 											</li>
 										)
-									})}
+									)}
 								</ul>
 							</li>
 						)
-					})}
+					)}
 				</ul>
 			</div>
 		)
