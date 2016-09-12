6ba9f082e4847cc22802ac4e6155131cd0de9a67
Gabriel Florit <gabrielflorit@gmail.com>
2016-09-12 18:38:08 -0400

    Document President.js

--- common/containers/President.js
+++ common/containers/President.js
@@ -1,3 +1,5 @@
+// The `President` class displays presidential results for both US and MA.
+
 import React, { Component, PropTypes } from 'react'
 import { provideHooks } from 'redial'
 import { bindActionCreators } from 'redux'
@@ -8,16 +10,33 @@ import ElectoralCollegeBar from './../components/ElectoralCollegeBar.js'
 import ElectoralCollegeMap from './../components/ElectoralCollegeMap.js'
 import MassMap from './../components/MassMap.js'
 
+// This object, used by the `@provideHooks` decorator, defines our custom
+// data loading dependencies. At the moment we just have one: `fetch`. It
+// gets triggered on both server and client.
 const hooks = {
+
+	// `fetch` takes a `dispatch` argument, which we will use to fire
+	// our custom data loading actions. In this case, we dispatch the
+	// `fetchResults` action.
 	fetch: ({ dispatch }) =>
 		dispatch(actions.fetchResults({ url: 'president' })),
 }
 
+// This object maps various properties as React `props`:
 const mapDispatchToProps = (dispatch) => ({
-	dispatch,
+
+	// `actions` which have already been bound with `dispatch` for convenience,
 	actions: bindActionCreators(actions, dispatch),
+
+	// and `dispatch`, so we can pass it to `fetch` above. Normally we
+	// wouldn't set `dispatch` as a React `props`, but we need it to fire
+	// our custom `fetch` lifecycle event above.
+	dispatch,
+
 })
 
+// Decorate the `President` class with `connect` and `provideHooks`, in that
+// order (it matters).
 @provideHooks(hooks)
 @connect(s => s, mapDispatchToProps)
 class President extends Component {
@@ -29,23 +48,28 @@ class President extends Component {
 		dispatch: PropTypes.func.isRequired,
 	}
 
-	// when component is initially mounted onto DOM, fire its 'fetch' hook
+	// This lifecycle event gets called once, immediately after the initial
+	// rendering occurs. We will use it to fire the `fetch` hook.
 	componentDidMount = () => {
 		this.fetchData()
 	}
 
+	// This gets called once after the component's updates are flushed to DOM.
+	// At the moment we will use it to determine whether to stop the clock.
 	componentDidUpdate = (prevProps) => {
 
 		const { props } = this
 		const { startTimer, cancelTimer } = props.actions
 
-		// did we stop fetching?
+		// Did we stop fetching - that is, did we go from `isFetching == true`
+		// to `isFetching == false`? If so,
 		if (prevProps.results.isFetching && !props.results.isFetching) {
 
-			// TODO: add data completeness check
+			// check to see if all results are in so we can stop the clock.
+
+			// TODO: implement
 			++this.count
 
-			// is the race over?
 			if (this.count > 2) {
 				cancelTimer()
 			} else {
@@ -56,10 +80,12 @@ class President extends Component {
 
 	}
 
+	// Wrap the `fetch` call in a simpler `fetchData` function.
 	fetchData = () => {
 		hooks.fetch({ dispatch: this.props.dispatch })
 	}
 
+	// TODO: remove when we implement data completion check.
 	count = 0
 
 	render() {
@@ -68,14 +94,23 @@ class President extends Component {
 		const { timer, results, selection } = props
 		const { stopTimer, selectTown } = props.actions
 
+		// Prepare `Timer` props, including
 		const timerProps = {
 			...timer,
+
+			// a callback which gets invoked when the `Timer` runs out. If so,
 			callback: () => {
+
+				// fire the `stopTimer` action,
 				stopTimer()
+
+				// and fire the `fetch` hook, which will fire the `fetchResults`
+				// action.
 				fetchData()
 			},
 		}
 
+		// Finally we can render all the components!
 		return (
 			<div className='President'>
 				<h1>President</h1>
