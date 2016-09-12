5756df8cc4de2d8d6421b546f3a897793cf32e2c
Gabriel Florit <gabrielflorit@gmail.com>
2016-09-12 17:50:03 -0400

    Document client/index.js

--- client/index.js
+++ client/index.js
@@ -1,39 +1,46 @@
+/* eslint-disable global-require, no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { browserHistory } from 'react-router'
 import configureStore from './../common/store/configureStore.js'
-
-// eslint-disable-next-line no-unused-vars
 import css from './../common/styles/config.styl'
 
+// Grab the initial Redux state (a json string created by the server).
 const initialState = window.REDUX__INITIALSTATE
+
+// Hydrate the Redux store with `initialState`.
 const store = configureStore(initialState)
+
 const rootElement = document.getElementById('root')
 
-// the following code (render, module.hot if, etc) comes from
-// https://github.com/reactjs/redux/pull/1455 (cleaner than react-hmre)
+// Setup a `render` function that we will overwrite for hot module
+// reloading - see https://github.com/reactjs/redux/pull/1455.
 let render = () => {
 
-	// eslint-disable-next-line global-require
 	const Root = require('./../common/components/Root.js').default
 
+	// Render React app to `rootElement`.
 	ReactDOM.render(
 		<Root history={browserHistory} store={store} />, rootElement)
 
 }
 
+// If HMR is enabled,
 if (module.hot) {
 
 	const renderRoot = render
+
+	// and we have an error,
 	const renderError = (error) => {
 
-		// eslint-disable-next-line global-require
 		const RedBox = require('redbox-react')
 
+		// render error with `RedBox` styling.
 		ReactDOM.render(<RedBox error={error} />, rootElement)
 
 	}
 
+	// Otherwise try to render app normally.
 	render = () => {
 
 		try {
