f5669e87d1a21cf6fdbb55f04edc58bbf59a5dc9
Gabriel Florit <gabrielflorit@gmail.com>
2016-09-15 01:40:13 -0400

    Add comments

--- common/components/StateResultsTable.js
+++ common/components/StateResultsTable.js
@@ -3,14 +3,33 @@
 
 import React, { PropTypes } from 'react'
 
+// TODO: implement
+const createSummary = (raceName) =>
+	// eslint-disable-next-line max-len
+	`A table that has the candidate percent and vote count across the top and the states down the left hand side for the ${raceName}.`
+
 const StateResultsTable = ({ race }) => {
 
 	console.log(race)
 
+	// How do we decide on a sorting order for these candidates?
+	// By electoral leading? electoral won?
+
 	return (
-		<pre>
-			{ JSON.stringify(race, null, 2)}
-		</pre>
+		<div>
+			<h1>State by state results</h1>
+			<table summary={createSummary()}>
+				<thead>
+					<tr>
+						<th scope='col'>
+							<div>State</div>
+							<div>Precincts reporting</div>
+						</th>
+
+					</tr>
+				</thead>
+			</table>
+		</div>
 	)
 }
 
