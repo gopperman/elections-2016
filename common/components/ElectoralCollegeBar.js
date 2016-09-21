49991875d1756ded8e9a291e8b91056f45c229ef
Gabriel Florit <gabrielflorit@gmail.com>
2016-09-21 18:35:08 -0400

    Port ElectoralCollegeBar to new API data

--- common/components/ElectoralCollegeBar.js
+++ common/components/ElectoralCollegeBar.js
@@ -1,62 +1,57 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import _ from 'lodash'
 
-// TODO: remove all unnecessary prefer-stateless-functions disables
-// eslint-disable-next-line react/prefer-stateless-function
-export default class ElectoralCollegeBar extends Component {
+// TODO: sanitize data (cast strings to numbers)
+const ElectoralCollegeBar = ({ reportingUnits, test }) => {
 
-	static propTypes = {
-		data: PropTypes.object.isRequired,
-	}
+	const { candidates, precinctsReportingPct } = reportingUnits[0]
 
-	render() {
+	const dem = _.find(candidates, { party: 'Dem' })
+	const gop = _.find(candidates, { party: 'GOP' })
+	const totalWon = _.sumBy(candidates, v => +v.electWon)
+	const undecided = 538 - totalWon
 
-		const { data } = this.props
+	return (
+		<div className='ElectoralCollegeBar'>
+			<h1>ElectoralCollegeBar</h1>
+			<h2>test: {test.toString()}</h2>
 
-		const { Sumtable } = data
-		const candidates = (Sumtable && Sumtable.Cand) || []
-
-		// TODO: do we want to incorporate "leading"?
-		const dem = _.find(candidates, { party: 'Dem' }) || {}
-		const gop = _.find(candidates, { party: 'GOP' }) || {}
-		const totalWon = _.sumBy(candidates, x => +x.ElectWon)
-		const undecided = 538 - totalWon
-
-		return (
-			<div className='ElectoralCollegeBar'>
-				<h1>ElectoralCollegeBar</h1>
-
-				<div>
-					<h3>Dem</h3>
-					<ul>
-						<li>Name: {dem.name}</li>
-						<li>Is winner: {dem.Winner}</li>
-						<li>Electoral votes won: {dem.ElectWon}</li>
-						<li>Popular votes: {dem.PopVote}</li>
-					</ul>
-				</div>
-
-				<div>
-					<h3>GOP</h3>
-					<ul>
-						<li>Name: {gop.name}</li>
-						<li>Is winner: {gop.Winner}</li>
-						<li>Electoral votes won: {gop.ElectWon}</li>
-						<li>Popular votes: {gop.PopVote}</li>
-					</ul>
-				</div>
-
-				<div>
-					Precincts reported: {Sumtable.PrecinctsPct}%
-				</div>
-
-				<div>
-					Undecided: {undecided}
-				</div>
-
+			<div>
+				<h3>Dem</h3>
+				<ul>
+					<li>Name: {dem.last}</li>
+					<li>Is winner: {dem.winner}</li>
+					<li>Electoral votes won: {dem.electWon}</li>
+					<li>Popular votes: {dem.voteCount}</li>
+				</ul>
 			</div>
-		)
 
-	}
+			<div>
+				<h3>GOP</h3>
+				<ul>
+					<li>Name: {gop.last}</li>
+					<li>Is winner: {gop.winner}</li>
+					<li>Electoral votes won: {gop.electWon}</li>
+					<li>Popular votes: {gop.voteCount}</li>
+				</ul>
+			</div>
+
+			<div>
+				Precincts reported: {precinctsReportingPct}%
+			</div>
+
+			<div>
+				Undecided: {undecided}
+			</div>
+
+		</div>
+	)
 
 }
+
+ElectoralCollegeBar.propTypes = {
+	reportingUnits: PropTypes.array.isRequired,
+	test: PropTypes.bool.isRequired,
+}
+
+export default ElectoralCollegeBar
