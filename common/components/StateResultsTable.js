c12c841c4794df94867ffef96bd1130b1b1bc443
Gabriel Florit <gabrielflorit@gmail.com>
2016-09-21 19:52:17 -0400

    Refactor State Table and Row to ingest new data

--- common/components/StateResultsTable.js
+++ common/components/StateResultsTable.js
@@ -1,52 +1,39 @@
-// The `StateResultsTable` class displays detailed results for each
-// state in the race.
-
 import React, { PropTypes } from 'react'
 import StateResultsTableRow from './StateResultsTableRow.js'
-import { formatStateAsReportingUnit } from './../utils/standardize.js'
 
 // TODO: implement
 const createSummary = (raceName) =>
 	// eslint-disable-next-line max-len
 	`A table that has the candidate percent and vote count across the top and the states down the left hand side for the ${raceName}.`
 
-const StateResultsTable = ({ race, summaryCandidates }) => {
-
-	// TODO: order states by their full name.
-	const rows = race.PresStateByStatetable.State
-		.map(formatStateAsReportingUnit)
-		.map((state, key) => (
-			<StateResultsTableRow {...{ state, summaryCandidates, key }} />
-		))
-
-	return (
-		<div>
-			<h1>State by state results</h1>
-			<table summary={createSummary()}>
-				<thead>
-					<tr>
-						<th scope='col'>
-							<div>State</div>
-							<div>Precincts reporting</div>
+const StateResultsTable = ({ states, summaryCandidates }) =>
+	<div>
+		<h1>State by state results</h1>
+		<table summary={createSummary()}>
+			<thead>
+				<tr>
+					<th scope='col'>
+						<div>State</div>
+						<div>Precincts reporting</div>
+					</th>
+					{ summaryCandidates.map((v, i) => (
+						<th scope='col' key={i}>
+							<div>{v.last}</div>
+							<div>{i === 0 ? 'Votes' : ''}</div>
 						</th>
-						{ summaryCandidates.map((c, i) => (
-							<th scope='col' key={i}>
-								<div>{c.last}</div>
-								<div>{i === 0 ? 'Votes' : ''}</div>
-							</th>
-						))}
-					</tr>
-				</thead>
-				<tbody>
-					{rows}
-				</tbody>
-			</table>
-		</div>
-	)
-}
+					))}
+				</tr>
+			</thead>
+			<tbody>
+				{ states.map((state, key) => (
+					<StateResultsTableRow key={key} {...state.reportingUnits[0]} />
+				))}
+			</tbody>
+		</table>
+	</div>
 
 StateResultsTable.propTypes = {
-	race: PropTypes.object.isRequired,
+	states: PropTypes.array.isRequired,
 	summaryCandidates: PropTypes.array.isRequired,
 }
 
--- common/components/StateResultsTableRow.js
+++ common/components/StateResultsTableRow.js
@@ -1,62 +1,29 @@
-// The `StateResultsTableRow` class displays detailed results for a state row
-// within a `StateResultsTable`.
-
-import _ from 'lodash'
 import React, { PropTypes } from 'react'
 import addCommas from 'add-commas'
 import { percent } from './../utils/Candidate.js'
 import { percentForDisplay } from './../utils/standardize.js'
-import { sortByIDs } from './../utils/Candidates.js'
 
-const StateResultsTableRow = ({ state, summaryCandidates }) => {
-
-	// Get this state's candidates
-	const { candidates } = state
-
-	// Get candidate ids for the overall candidates (winner order).
-	const candidateIDs = _.map(summaryCandidates, 'candidateID')
-
-	// Sort the state candidates by overall order.
-	const sortedCandidates = sortByIDs({ candidates, candidateIDs })
-
-	// Create the candidate tds.
-	const tds = sortedCandidates.map((candidate, i, array) => {
-
-		const { candidateID, voteCount } = candidate
-
-		// Add appropriate commas to the candidate's vote count.
-		const vote = addCommas(voteCount)
-
-		// Get the candidate's vote percent.
-		const pct = percent({ candidates: array, candidateID })
-
-		// Get the display-ready version of the percent.
-		const pctForDisplay = percentForDisplay(pct)
-
-		return (
+const StateResultsTableRow = ({ statePostal, precinctsReportingPct,
+candidates }) =>
+	<tr>
+		<td>
+			<div>{statePostal}</div>
+			<div>{precinctsReportingPct}%</div>
+		</td>
+		{ candidates.map((v, i) => (
 			<td key={i}>
-				<div>{pctForDisplay}%</div>
-				<div>{vote}</div>
+				<div>{percentForDisplay(percent({
+					candidates, candidateID: v.candidateID }))}%</div>
+				<div>{addCommas(v.voteCount)}</div>
+				<div>{v.last}</div>
 			</td>
-		)
-
-	})
-
-	// Create the entire row.
-	return (
-		<tr>
-			<td>
-				<div>{state.statePostal}</div>
-				<div>{state.precinctsReportingPct}%</div>
-			</td>
-			{tds}
-		</tr>
-	)
-}
+		))}
+	</tr>
 
 StateResultsTableRow.propTypes = {
-	state: PropTypes.object.isRequired,
-	summaryCandidates: PropTypes.array.isRequired,
+	candidates: PropTypes.array.isRequired,
+	statePostal: PropTypes.string.isRequired,
+	precinctsReportingPct: PropTypes.string.isRequired,
 }
 
 export default StateResultsTableRow
