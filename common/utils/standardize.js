91df021c11a37cb4e7914fd7922cf68a8587db4f
Gabriel Florit <gabrielflorit@gmail.com>
2016-09-23 19:22:29 -0400

    Fix broken tests

--- common/utils/standardize.js
+++ common/utils/standardize.js
@@ -1,51 +1,3 @@
-const formatElectoralSummary = (summary) => ({
-	candidates: summary.Cand.map(v => {
-
-		const result = {
-			electLead: +v.ElectLead,
-			electWon: +v.ElectWon,
-			popPct: +v.PopPct,
-			voteCount: +v.PopVote,
-			statesLead: +v.StatesLead,
-			statesWon: +v.StatesWon,
-			winner: v.Winner,
-			last: v.name,
-		}
-
-		if (v.CandID) result.candidateID = v.CandID
-		if (v.party) result.party = v.party
-
-		return result
-
-	}),
-	precinctsReportingPct: +summary.PrecinctsPct,
-	test: summary.Test,
-	office: summary.office,
-	timestamp: summary.timestamp,
-})
-
-const formatStateAsReportingUnit = (unit) => ({
-	candidates: unit.Cand.map(v => {
-
-		const result = {
-			candidateID: v.CandID,
-			electWon: +v.ElectWon,
-			popPct: +v.PopPct,
-			voteCount: +v.PopVote,
-			last: v.name,
-			party: v.party,
-		}
-
-		if (v.Winner) result.winner = v.Winner
-
-		return result
-
-	}),
-	electTotal: +unit.ElectTotal,
-	statePostal: unit.PostalCode,
-	precinctsReportingPct: +unit.PrecinctsPct,
-})
-
 const toSentenceCase = (s) =>
 	[s[0].toUpperCase(), s.slice(1)].join('')
 
@@ -70,6 +22,4 @@ export {
 	// eslint-disable-next-line import/prefer-default-export
 	percentForDisplay,
 	toSentenceCase,
-	formatStateAsReportingUnit,
-	formatElectoralSummary,
 }
