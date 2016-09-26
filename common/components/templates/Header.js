48fd4f2eb46254cf8001b927a262f243c1497dcd
Greg Opperman <gopperman@gmail.com>
2016-09-26 14:29:46 -0400

    Basic navigation

--- common/components/templates/Header.js
+++ common/components/templates/Header.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react'
 import ElectoralCollegeBar from './../ElectoralCollegeBar.js'
+import Navigation from './Navigation.js'
 
 const Header = ({ summaryState }) => (
 	<div className='header' key='header'>
 		<h1>Boston Globe</h1>
 		<h2>Elections 2016</h2>
+		<Navigation />
 		<ElectoralCollegeBar {...summaryState} />
 	</div>
 )
