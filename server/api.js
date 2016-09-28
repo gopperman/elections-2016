3c8c33facc4451caf97ae4b441d63730a71b732e
Gabriel Florit <gabrielflorit@gmail.com>
2016-09-28 15:42:39 -0400

    Keep api around for old pages

--- server/api.js
+++ server/api.js
@@ -1,18 +1,75 @@
+// TODO: remove this file
+import { readFileSync } from 'jsonfile'
+
 const { fetch } = require('fetch-ponyfill')()
+
+const readJson = (endpoint) =>
+	readFileSync(`./data/${endpoint}.json`)
 
 export default (req, res) => {
 
-	// Remove first bit
-	const baseUrl = req.originalUrl.replace(/\/api\//, '')
+	const { endpoint } = req.params
 
-	const url =
-		`http://devweb.bostonglobe.com/electionapi/elections/${baseUrl}`
+	console.log(`requesting ${endpoint}`)
 
-	console.log(`requesting ${url}`)
+	let result
+	switch (endpoint) {
 
-	fetch(url)
-		.then(response => response.json())
-		.then(json => res.json(json))
-		.catch(e => console.error(e))
+		case 'election': {
+			result = {
+				'president-us-states': readJson('president-us-states-0'),
+			}
+			console.log('about to send election home data')
+
+			setTimeout(() => res.json(result), 0)
+			break
+		}
+
+		case 'race': {
+
+			console.log('requesting race')
+
+			result = {
+				'president-us-states': readJson('president-us-states-0'),
+				'senate-ma-towns': readJson('senate-ma-towns'),
+			}
+
+			console.log('about to send race data')
+
+			setTimeout(() => res.json(result), 0)
+			break
+		}
+
+		case 'town': {
+			result = {
+				'president-us-states': readJson('president-us-states-0'),
+				'town-abington': readJson('town-abington'),
+			}
+			console.log('about to send town data')
+
+			setTimeout(() => res.json(result), 0)
+			break
+		}
+
+		default: {
+
+			// Remove first bit
+			const baseUrl = req.originalUrl.replace(/\/api\//, '')
+
+			const url =
+				`http://devweb.bostonglobe.com/electionapi/elections/${baseUrl}`
+
+			console.log(`requesting ${url}`)
+
+			fetch(url)
+				.then(response => response.json())
+				.then(json => res.json(json))
+				.catch(e => console.error(e))
+
+			break
+
+		}
+
+	}
 
 }
