b0f1fb332c5e79f2bef6c93d2bf78ccfcea51baf
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-06 13:58:35 -0400

    Start bringing over React lifecycle to BalanceOfPower

--- common/components/BalanceOfPower.js
+++ common/components/BalanceOfPower.js
@@ -1,6 +1,11 @@
+// The `BalanceOfPower` class displays senate balance of power results.
+
+/* eslint-disable no-return-assign */
+
 import React, { Component, PropTypes } from 'react'
 import * as d3 from 'd3'
-import { buildSeats } from './../utils/visUtils.js'
+
+// import { buildSeats } from './../utils/visUtils.js'
 
 class BalanceOfPower extends Component {
 
@@ -9,47 +14,99 @@ class BalanceOfPower extends Component {
 		gop: PropTypes.number.isRequired,
 	}
 
+	// This lifecycle event gets called once, immediately after the initial
+	// rendering occurs.
 	componentDidMount() {
-		// Create margins.
-		const container = document.getElementById('root')
-		const margin = { top: 10, right: 10, bottom: 10, left: 10 }
-		const outerWidth = container.offsetWidth
+
+		// Create outer width from container.
+		const outerWidth = this._svg.parentNode.offsetWidth
+
+		// Set outer height from outer width.
 		const outerHeight = outerWidth
+
+		// Create margins and inner dimensions.
+		const margin = { top: 10, right: 10, bottom: 10, left: 10 }
 		const width = outerWidth - margin.left - margin.right
 		const height = outerHeight - margin.top - margin.bottom
-		const baseRadius = 80
 
-		const senate = buildSeats(this.props.dem, this.props.gop, 100, 4)
+		// Set viewBox on svg.
+		d3.select(this._svg).attr('viewBox', `0 0 ${width} ${height}`)
 
-		// Create svg.
-		const svg = d3.select('.balanceOfPower').append('svg')
-				.attr('width', width + margin.left + margin.right)
-				.attr('height', height + margin.top + margin.bottom)
-			.append('g')
-				.attr('transform', `translate(${outerWidth/2}, ${outerHeight/2})`)
+		// Draw chart (although at this point we might not have data).
+		this.drawChart()
 
-		svg.selectAll('.seat')
-				.data(senate)
-			.enter().append('g')
-				.each(function(d, i) {
-						d3.select(this).selectAll('.seat')
-						.data(d)
-					.enter().append('circle')
-						.attr('cx', (d, j) => 
-							(baseRadius + (i+1) * 16) * Math.cos(Math.PI/(senate[0].length - 1) * j)
-						)
-						.attr('cy', (d, j) => 
-							-((baseRadius + (i+1) * 16) * Math.sin(Math.PI/(senate[0].length - 1) * j))
-						)
-						.attr('r', 5)
-						.attr("class", (d) => ('.balance__circle--' + d.party))
-				})
+// 		// Create margins.
+// 		const container = document.getElementById('root')
+// 		const outerWidth = container.offsetWidth
+// 		const outerHeight = outerWidth
+
+// 		const width = outerWidth - margin.left - margin.right
+// 		const height = outerHeight - margin.top - margin.bottom
+// 		const baseRadius = 80
+
+// 		const senate = buildSeats(this.props.dem, this.props.gop, 100, 4)
+
+// 		// Create svg.
+// 		const svg = d3.select('.balanceOfPower').append('svg')
+// 				.attr('width', width + margin.left + margin.right)
+// 				.attr('height', height + margin.top + margin.bottom)
+// 			.append('g')
+// 				.attr('transform', `translate(${outerWidth/2}, ${outerHeight/2})`)
+
+// 		svg.selectAll('.seat')
+// 				.data(senate)
+// 			.enter().append('g')
+// 				.each(function(d, i) {
+// 						d3.select(this).selectAll('.seat')
+// 						.data(d)
+// 					.enter().append('circle')
+// 						.attr('cx', (d, j) =>
+// 							(baseRadius + (i+1) * 16) * Math.cos(Math.PI/(senate[0].length - 1) * j)
+// 						)
+// 						.attr('cy', (d, j) =>
+// 							-((baseRadius + (i+1) * 16) * Math.sin(Math.PI/(senate[0].length - 1) * j))
+// 						)
+// 						.attr('r', 5)
+// 						.attr("class", (d) => ('.balance__circle--' + d.party))
+// 				})
+
+	}
+
+	// This is invoked before rendering when new props or state are being
+	// received. This method is not called for the initial render or when
+	// `forceUpdate` is used.
+	shouldComponentUpdate(nextProps) {
+
+		// Update component if `dem` or `gop` has changed.
+		const { dem, gop } = this.props
+		const newDem = nextProps.dem
+		const newGop = nextProps.gop
+
+		// TODO: implement
+		return false
+
+	}
+
+	// This is invoked immediately after the component's updates are flushed
+	// to the DOM. This method is not called for the initial render.
+	componentDidUpdate() {
+
+		// After the component updates, draw chart.
+		this.drawChart()
+
+	}
+
+	drawChart = () => {
+
+		console.log('here we will draw the chart')
+
 	}
 
 	render() {
 		return (
 			<div className='balanceOfPower'>
 				<h1>Balance of Power</h1>
+				<svg ref={(c) => this._svg = c} />
 			</div>
 		)
 
