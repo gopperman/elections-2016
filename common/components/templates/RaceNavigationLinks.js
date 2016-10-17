6ad8660a7d4b70be75b923430d45894b8415377a
Greg Opperman <gopperman@gmail.com>
2016-10-17 13:16:56 -0400

    Race navigation links in the header, and a common component for both header and footer

---
+++ common/components/templates/RaceNavigationLinks.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const RaceNavigationLinks = () => (
+	<ul className="raceNavigationLinks">
+		<li><a href='/elections/2016/president'>President</a></li>
+		<li><a href='/elections/2016/office/us-house'>US House</a></li>
+		<li><a href='/elections/2016/office/us-senate'>US Senate</a></li>
+		<li><a href='#'>Mass. ballot questions</a></li>
+		<li><a href='/elections/2016/office/ma-senate'>State Senate</a></li>
+		<li><a href='/elections/2016/office/ma-senate'>State House</a></li>
+		<li><a href='/elections/2016/president'>President</a></li>
+		<li><a href='/elections/2016/office/governors-council'>Governor’s Council</a></li>
+		<li><a href='/elections/2016/office/register-of-deeds'>Register of deeds</a></li>
+		<li><a href='/elections/2016/office/county-commissioner'>County Commissioner</a></li>
+		<li><a href='/elections/2016/office/sheriff'>Sheriff</a></li>
+	</ul>
+)
+
+export default RaceNavigationLinks
