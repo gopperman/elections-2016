9696835dc2ba7d67f80bb890aed34560a9355af8
Greg Opperman <gopperman@gmail.com>
2016-10-17 13:16:56 -0400

    Race navigation links in the header, and a common component for both header and footer

--- common/components/templates/Navigation.js
+++ common/components/templates/Navigation.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import RaceNavigationLinks from './RaceNavigationLinks.js'
 import TownLookup from '../TownLookup.js'
+
 
 const Navigation = () => (
 	<div className='nav' key='nav'>
 		<ul className='nav__links'>
 			<li><a href='/election'>Elections</a></li>
-			<li><a href='/town/abington'>By Town</a></li>
-			<li><a href='/race'>By Race</a></li>
+			<li><a href='#'>By Town</a></li>
+			<li>
+				<a href='/race'>By Race</a>
+				<RaceNavigationLinks />
+			</li>
 			<li><a href='/president'>President</a></li>
 		</ul>
 		<TownLookup />
