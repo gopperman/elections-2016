6d48674d5ad2ae6c7b6baea6761f160f55f7dacf
elaina <elaina.natario@gmail.com>
2016-10-20 17:24:36 -0400

    basic main nav styling

--- common/components/Navigation.js
+++ common/components/Navigation.js
@@ -6,17 +6,17 @@ const Navigation = () => (
 	<nav className='g-nav' key='nav'>
 		<ul className='g-nav__list'>
 			<li className='g-nav__item'>
-				<a className='g-nav__link benton-bold' href='/election'>Elections 2016</a>
+				<a className='g-nav__link benton-bold icon icon--election' href='/election'>Elections 2016</a>
 			</li>
 			<li className='g-nav__item'>
-				<a className='g-nav__link benton-bold' href='/town'>Town results</a>
+				<a className='g-nav__link benton-bold icon icon--town' href='/town'>Town results</a>
 			</li>
 			<li className='g-nav__item g-nav__item--subnav'>
-				<a className='g-nav__link benton-bold' href='/race'>Find a race</a>
+				<a className='g-nav__link benton-bold icon icon--race' href='/race'>Find a race</a>
 				{/* <RaceNavigationLinks /> */}
 			</li>
 			<li className='g-nav__item'>
-				<a className='g-nav__link benton-bold' href='/president'>Presidential race</a>
+				<a className='g-nav__link benton-bold icon icon--president' href='/president'>Presidential race</a>
 			</li>
 		</ul>
 	</nav>
