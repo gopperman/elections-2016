de0eabc04bb5169ad3bea542c2492bddfef33cfa
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-20 18:07:33 -0400

    Add urlManager to Navigation.js, closes #223

--- common/components/Navigation.js
+++ common/components/Navigation.js
@@ -1,22 +1,31 @@
 import React from 'react'
 // import RaceNavigationLinks from './RaceNavigationLinks.js'
 // import TownLookup from '../TownLookup.js'
+import urlManager from './../utils/urlManager.js'
 
 const Navigation = () => (
 	<nav className='g-nav' key='nav'>
 		<ul className='g-nav__list'>
 			<li className='g-nav__item'>
-				<a className='g-nav__link benton-bold icon icon--election' href='/election'>Elections 2016</a>
+				<a
+					className='g-nav__link benton-bold icon icon--election'
+					href={`/${urlManager.base()}`}>Elections 2016</a>
 			</li>
 			<li className='g-nav__item'>
-				<a className='g-nav__link benton-bold icon icon--town' href='/town'>Town results</a>
+				<a
+					className='g-nav__link benton-bold icon icon--town'
+					href={`/${urlManager.base()}/town/boston`}>Town results</a>
 			</li>
 			<li className='g-nav__item g-nav__item--subnav'>
-				<a className='g-nav__link benton-bold icon icon--race' href='/race'>Find a race</a>
+				<a
+					className='g-nav__link benton-bold icon icon--race'
+					href={`/${urlManager.base()}/race/Question/4 - Legalize Marijuana`}>Find a race</a>
 				{/* <RaceNavigationLinks /> */}
 			</li>
 			<li className='g-nav__item'>
-				<a className='g-nav__link benton-bold icon icon--president' href='/president'>Presidential race</a>
+				<a
+					className='g-nav__link benton-bold icon icon--president'
+					href={`/${urlManager.base()}/president`}>Presidential race</a>
 			</li>
 		</ul>
 	</nav>
