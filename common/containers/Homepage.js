b0e8c8e021ee9debd06465e020e1e06c971c468d
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-22 19:45:19 -0400

    Make homepage like election

--- common/containers/Homepage.js
+++ common/containers/Homepage.js
@@ -1,34 +1,18 @@
-// The `Homepage` class displays homepage content.
-
-import { geoAlbersUsa } from 'd3-geo'
 import _ from 'lodash'
-import React, { Component, PropTypes } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import * as topojson from 'topojson'
-
-import * as actions from './../actions/actionCreators.js'
+import { geoAlbersUsa } from 'd3-geo'
+import React, { Component } from 'react'
+import connectToApi from './connectToApi.js'
 import Timer from './../components/Timer.js'
+import Header from './../components/Header.js'
+import Footer from './../components/Footer.js'
+import TestStatus from './../components/TestStatus.js'
+import ElectoralCollegeBar from './../components/ElectoralCollegeBar.js'
 import Map from './../components/Map.js'
 import STATES from './../../data/output/STATES.json'
-import Header from './../components/Header.js'
-import TestStatus from './../components/TestStatus.js'
-import SwingStates from './../components/SwingStates.js'
-import ResultDualBar from './../components/ResultDualBar.js'
-
-import {
-	sortByElectoralCount,
-	sortByPolIDs,
-} from './../utils/Candidates.js'
-
-// This object maps various properties as React `props`:
-const mapDispatchToProps = (dispatch) => ({
-
-	// `actions` which have already been bound with `dispatch`
-	// for convenience.
-	actions: bindActionCreators(actions, dispatch),
-
-})
+import { sortByElectoralCount } from './../utils/Candidates.js'
+import FeatureGroup from './../components/FeatureGroup.js'
+import LinkButton from './../components/LinkButton.js'
+import urlManager from './../utils/urlManager.js'
 
 // We'll keep these urls here for testing. A description:
 
@@ -36,204 +20,104 @@ const mapDispatchToProps = (dispatch) => ({
 // const url = 'NO'
 
 // this one returns json but the data is incomplete,
-// const url = '016-11-08?officeID=P'
+// const url = '2016-11-08/rezcentral'
 
 // and this one is the correct url - it returns everything.
-// const url = '2016-11-08?officeID=P'
-const url = 'homepage'
+const url = '2016-11-08/prezcentral?races=MA-22949,MA-24805'
 
-@connect(s => s, mapDispatchToProps)
+@connectToApi
 class Homepage extends Component {
 
-	static propTypes = {
-		actions: PropTypes.object.isRequired,
-		timer: PropTypes.object.isRequired,
-		results: PropTypes.object.isRequired,
+	static apiUrl() {
+		return url
 	}
 
-	// This lifecycle event gets called once, immediately after the initial
-	// rendering occurs. We will use it to fire the `fetch` hook.
-	componentDidMount = () => {
-		this.fetchData()
-	}
+	static areAllRacesComplete() {
 
-	// This gets called once after the component's updates are flushed to DOM.
-	// At the moment we will use it to determine whether to stop the clock.
-	componentDidUpdate = (prevProps) => {
-
-		const { props } = this
-		const { startTimer, cancelTimer } = props.actions
-		const { results } = props
-
-		// Did we stop fetching - that is, did we go from `isFetching == true`
-		// to `isFetching == false`? If so:
-		if (prevProps.results.isFetching && !props.results.isFetching) {
-
-			// Get the data - or an empty object.
-			const data = results.data || {}
-
-			// Get API results.
-			const races = data.races || []
-
-			// Get US race:
-			const presidentRaceStates = races
-				// only include 'P' (President) races,
-				.filter(v => v.officeID === 'P')
-				// return the first item of reportingUnits,
-				.map(v => (v.reportingUnits || [])[0])
-				// and don't include null items.
-				.filter(v => v)
-
-			// Get US presidential race summary.
-			const summaryState =
-				_.find(presidentRaceStates, { statePostal: 'US' })
-
-			// Check if all results are in.
-			const isFinished = +summaryState.precinctsReportingPct === 100
-
-			if (isFinished) {
-				cancelTimer()
-			} else {
-				startTimer()
-			}
-
-		}
-
-	}
-
-	// Encapsulate data fetching logic.
-	fetchData = () => {
-		this.props.actions.fetchResults({ url })
+		// TODO: implement
+		return true
 	}
 
 	render() {
 
-		const { props, fetchData } = this
-		const { timer, results } = props
-		const { stopTimer } = props.actions
-
-		// Prepare `Timer` props, including
-		const timerProps = {
-			...timer,
-
-			// a callback which gets invoked when the `Timer` runs out. If so,
-			callback: () => {
-
-				// fire the `stopTimer` action,
-				stopTimer()
-
-				// and fire the `fetch` hook, which will fire the `fetchResults`
-				// action.
-				fetchData()
-			},
-		}
+		const { props } = this
+		const { timerProps, results } = props
 
 		// Get the data - or an empty object.
 		const data = results.data || {}
 
-		// Get API results.
+		// Get all races.
 		const races = data.races || []
 
-		// Get president races.
-		const presidentRaces = races.filter(v => v.officeID === 'P')
-
-		// Get president race states.
-		const presidentRaceStates = presidentRaces
-			// return the first item of reportingUnits,
+		// Get all presidential races:
+		const presRaces = _(races)
+			// get all races where officeName='President',
+			.filter({ officeName: 'President' })
+			// get the first item of reportingUnits,
 			.map(v => (v.reportingUnits || [])[0])
 			// and don't include null items.
 			.filter(v => v)
+			.value()
+
+		const presUs = _.find(races,
+			{ officeName: 'President', statePostal: 'US' })
+
+		// Get presidential summary.
+		const presSummary = _.find(presRaces, { statePostal: 'US' })
+
+		// Get all 51 states.
+		const presStates = _.reject(presRaces, { statePostal: 'US' })
+
+		// Create the map (if we have data).
+		const map = presStates.length ? (<Map
+			shapefile={STATES}
+			data={presStates}
+			unitName='stateName'
+			projection={geoAlbersUsa()}
+			sortingDelegate={sortByElectoralCount}
+			dropdownName='state'
+			displayName='stateName'
+			labelsName='STUSPS' />) : null
 
 		// Get test status.
 		const isTest = _.some(races, 'test')
 
-		// Get US presidential race summary.
-		const summaryState =
-			_.find(presidentRaceStates, { statePostal: 'US' })
-
-		// Define the candidates we're interested in.
-		// TODO: is this necessary?
-		const mainCandidatePolIDs = ['1746', '8639', '31708', '895']
-
-		// Get summary US candidates, so we can sort by them.
-		// TODO: is this necessary?
-		const summaryStateCandidates = summaryState ?
-			sortByElectoralCount(summaryState.candidates)
-			.map(v => ({
-				...v,
-				isMainCandidate: _.includes(mainCandidatePolIDs, v.polID),
-			})) : []
-
-		// Prepare the US race so it can be easily ingested by sub-components:
-		const states = _(presidentRaceStates)
-			// don't include summary state,
-			.reject({ statePostal: 'US' })
-			// sort states by their full name,
-			.sortBy('stateName')
-			.map(v => ({
-				...v,
-				// and sort candidates by overall candidates.
-				candidates: sortByPolIDs({
-					candidates: v.candidates,
-					polIDs: _.map(summaryStateCandidates, 'polID'),
-				}),
-			}))
+		// Get featured races.
+		const featured = _(races)
+			.reject({ officeName: 'President' })
+			.map((race, key) => <FeatureGroup {...{ race, key }} />)
 			.value()
 
-		// Get swing states
-		const swingStateList = ['AZ', 'CO', 'IA']
-
-		// TODO: test that this works when swing state candidates are [] or null
-		const swingStates = _.filter(states, state =>
-			_.includes(swingStateList, state.statePostal))
-
-		// TODO: delete this when the API returns level: 'state' data
-		const massPresident = _(presidentRaces)
-			.filter({ statePostal: 'MA' })
-			.map(v => ({
-				...v,
-				reportingUnits: v.reportingUnits.map(w => ({
-					...w,
-					level: 'state',
-				})),
-			}))
-			.head()
-
-		const featuredRaces = _.difference(races, presidentRaces)
-			.concat(massPresident)
-			.filter(v => v)
-			.map((race, key) => <ResultDualBar {...{ race, key }} />)
-
 		return (
-			<div className='Homepage'>
+			<div>
 
 				<TestStatus isTest={isTest} />
 
-				<Header summaryState={summaryState} />
+				<Header />
 
-				<h1>Ohio hangs in balance</h1>
-				<h2>subhed</h2>
+				<main id='content'>
 
-				<Timer {...timerProps} />
+					<div className='container-lg'>
 
-				<SwingStates states={swingStates} />
+						<Timer {...timerProps} />
 
-				<Map
-					geoJson={topojson.feature(STATES, STATES.objects.UNITS)}
-					data={states}
-					sortingDelegate={sortByElectoralCount}
-					projection={geoAlbersUsa()}
-					unitName='stateName'
-					dropdownName='state'
-					labelsName='STUSPS'
-					displayName='stateName' />
+						<ElectoralCollegeBar {...presSummary} />
 
-				{featuredRaces}
+						{map}
 
-				<p>or big photo</p>
+						<LinkButton
+							text='See full results'
+							url={urlManager.race(presUs)} />
 
-				<p>summaries</p>
-				<p>selected races</p>
+						<div className='r-row--full'>
+							{featured}
+						</div>
+
+					</div>
+
+				</main>
+
+				<Footer />
 
 			</div>
 		)
