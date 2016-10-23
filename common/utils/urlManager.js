4ea9c5ec60a74b105e8fd12cdd7407d623f3acaa
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-23 16:06:34 -0400

    Make urlManager.race for president return the right url

--- common/utils/urlManager.js
+++ common/utils/urlManager.js
@@ -19,8 +19,18 @@ const urlManager = {
 	},
 
 	race(race) {
+
 		const { officeName, seatName } = race
-		return `${this.base()}/race/${officeName}/${seatName}`
+		let result
+
+		if (officeName.toLowerCase() === 'president') {
+			result = `${this.base()}/president`
+		} else {
+			result = `${this.base()}/race/${officeName}/${seatName}`
+		}
+
+		return result
+
 	},
 
 }
