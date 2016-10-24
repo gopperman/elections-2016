58fcf3a74aaaa24cee9a68dd709a39787da360b8
elaina <elaina.natario@gmail.com>
2016-10-24 12:43:22 -0400

    add in subnav onclick

--- common/components/Navigation.js
+++ common/components/Navigation.js
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import RaceNavigationLinks from './RaceNavigationLinks.js'
 import urlManager from './../utils/urlManager.js'
+import classNames from 'classNames'
+// import TownLookup from '../TownLookup.js'
 
-const Navigation = () => (
-	<nav className='g-nav' key='nav'>
-		<ul className='g-nav__list'>
-			<li className='g-nav__item'>
-				<a
-					className='g-nav__link benton-bold icon icon--election'
+class Navigation extends Component {
+	state = {
+		expanded: false,
+	}
+	raceOnClick = () => {
+		this.setState({expanded: !this.state.expanded}) 
+	}
+	render() {
+		const test = classNames('g-nav', { 'subnav-is-open': this.state.expanded })
+		return(
+		<nav className={test} key='nav'>
+			<ul className='g-nav__list'>
+				<li className='g-nav__item'>
+					<a className='g-nav__link benton-bold icon icon--election' 
 					href={urlManager.base()}>Elections 2016</a>
-			</li>
-			<li className='g-nav__item'>
-				<a
-					className='g-nav__link benton-bold icon icon--town'
-					href={`${urlManager.base()}/town/boston`}>Town results</a>
-			</li>
-			<li className='g-nav__item g-nav__item--subnav'>
-				<a
-					className='g-nav__link benton-bold icon icon--race'
-					href={urlManager.office('Question')}>Find a race</a>
-			</li>
-			<li className='g-nav__item'>
-				<a
-					className='g-nav__link benton-bold icon icon--president'
+				</li>
+				<li className='g-nav__item'>
+					<button className='g-nav__link benton-bold icon icon--town'>Town results</button>
+				</li>
+				<li className='g-nav__item g-nav__item--subnav'>
+					<button className='g-nav__link benton-bold icon icon--race' onClick={this.raceOnClick}>Find a race</button>
+					<RaceNavigationLinks />
+				</li>
+				<li className='g-nav__item'>
+					<a className='g-nav__link benton-bold icon icon--president' 
 					href={urlManager.office('President')}>Presidential race</a>
-			</li>
-		</ul>
-	</nav>
-)
+				</li>
+			</ul>
+		</nav>
+	)}}
 
 export default Navigation
