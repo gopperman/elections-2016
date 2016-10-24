adaaccc3c138dae4ebd53542e2abeea7aac82c62
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-24 19:06:16 -0400

    Add town dropdown to Navigation

--- common/components/TownLookup.js
+++ common/components/TownLookup.js
@@ -1,46 +1,37 @@
 import React, { Component } from 'react'
 import Select from 'react-select'
-import * as topojson from 'topojson'
-import TOWNS from './../../data/output/TOWNS.json'
-import { toSentenceCase } from './../utils/standardize.js'
+import getTownList from './../utils/getTownList.js'
+import urlManager from './../utils/urlManager.js'
 
-// TODO: verify this component uses the latest utils/architecture
 class TownLookup extends Component {
 
-	componentDidMount() {
+	state = {
+		value: null,
+	}
 
-		const geoJSON = topojson.feature(TOWNS, TOWNS.objects.UNITS)
-		this._townList = geoJSON.features.map(v => {
-			const town = v.id.toLowerCase()
-			return {
-				value: town,
-				label: toSentenceCase(town),
+	onChange = (value) => {
+
+		const location = typeof window !== 'undefined' && window.location
+		if (location) {
+			this.setState({ value })
+
+			if (value) {
+				location.href = urlManager.town(value.value)
 			}
-		})
+
+		}
 
 	}
 
-	updateValue(newValue) {
-		// TODO: only use window if on client
-		window.location.href = `/elections/2016/town/${newValue}`
-	}
-
-	render() {
-
-		return (
-			<div className='section'>
-				<h3 className='section-heading'>Select a town:</h3>
-				<Select
-					options={this._townList}
-					simpleValue
-					clearable
-					name='selected-state'
-					onChange={this.updateValue}
-					searchable
-				/>
-			</div>
-		)
-	}
+	render = () => (
+		<Select
+			placeholder='Select a town...'
+			value={this.state.value}
+			searchable
+			className='benton-regular'
+			onChange={this.onChange}
+			options={getTownList().map(v => ({ value: v, label: v }))} />
+	)
 
 }
 
