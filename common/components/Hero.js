437ceb4bc4da3de0a4378bc7fbfd7631a6058e08
elaina <elaina.natario@gmail.com>
2016-10-24 18:08:33 -0400

    hero styles

--- common/components/Hero.js
+++ common/components/Hero.js
@@ -1,17 +1,36 @@
 import React, { PropTypes } from 'react'
 import svgs from './../utils/svgs.js'
+import classnames from 'classnames'
 
-const Hero = ({ title }) => (
-	<div className='hero lead-bg'>
-		<h1 className='hed hero__hed benton-bold'>{title}</h1>
-		<div
-			className='hero__flourish'
-			dangerouslySetInnerHTML={{ __html: svgs.flourish }} />
-	</div>
-)
+const Hero = ({ title, isFeature }) => {
+
+	const mainClass = classnames('hero', {
+		'lead-bg': !isFeature,
+		'lead-img': isFeature,
+	})
+
+	const h1Class = classnames('hed', 'hero__hed', { 'benton-bold': !isFeature })
+
+	const h1 = isFeature ? (
+		<h1 className={h1Class}>
+			<span className="benton-regular">Election </span>
+			<span className="benton-bold">2016</span>
+		</h1>
+	) : (<h1 className={h1Class}>{title}</h1>)
+	
+	return (
+		<div className={mainClass}>
+			{h1}
+			<div
+				className='hero__flourish'
+				dangerouslySetInnerHTML={{ __html: svgs.flourish }} />
+		</div>
+	)
+}
 
 Hero.propTypes = {
 	title: PropTypes.string.isRequired,
+	isFeature: PropTypes.bool,
 }
 
 export default Hero
