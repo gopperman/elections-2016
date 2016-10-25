97f66f08c446a5d6be1cf0d8487ec7cef06bda72
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-24 21:03:06 -0400

    Add getSenateReport test

--- common/utils/dataUtil.test.js
+++ common/utils/dataUtil.test.js
@@ -1,16 +1,25 @@
 /* global describe, it */
 
 import assert from 'assert'
-// import { readFileSync } from 'jsonfile'
-// import { getSenateReport } from './dataUtil'
+import { readFileSync } from 'jsonfile'
+import { getSenateReport } from './dataUtil'
 
 describe('dataUtil', () => {
 
 	describe('getSenateReport', () => {
 
-		it('should work', () => {
+		it('should work with valid data', () => {
 
-			assert.equal(true, false)
+			const report = readFileSync('./data/report-trend-s.json')
+
+			const input = getSenateReport(report)
+			const output = [
+				{ party: 'dem', won: 13, leading: 0, holdovers: 34 },
+				{ party: 'gop', won: 19, leading: 2, holdovers: 30 },
+				{ party: 'ind', won: 0, leading: 0, holdovers: 2 },
+			]
+
+			assert.deepEqual(input, output)
 
 		})
 
