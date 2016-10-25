6ff71de92a4a7d7b72691b85411372f6c706e83c
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-24 20:47:56 -0400

    Add dataUtil test skeleton

---
+++ common/utils/dataUtil.test.js
@@ -0,0 +1,19 @@
+/* global describe, it */
+
+import assert from 'assert'
+// import { readFileSync } from 'jsonfile'
+// import { getSenateReport } from './dataUtil'
+
+describe('dataUtil', () => {
+
+	describe('getSenateReport', () => {
+
+		it('should work', () => {
+
+			assert.equal(true, false)
+
+		})
+
+	})
+
+})
