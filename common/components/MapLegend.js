4a931160429a3851927b8cf6ac1e69490f3c3626
elaina <elaina.natario@gmail.com>
2016-10-25 16:52:21 -0400

    fix #265

--- common/components/MapLegend.js
+++ common/components/MapLegend.js
@@ -4,34 +4,34 @@ import { standardizeParty } from './../utils/standardize.js'
 const MapLegend = ({ parties }) => (
 	<ul className='legend'>
 		<li className='legend__item'>
-			<p className='legend__subhed'>No Data</p>
+			<p className='legend__subhed benton-regular'>No Data</p>
 			<dl className='legend__deflist'>
-				<dt className='legend__term visually-hidden'>No Data</dt>
+				<dt className='legend__term benton-regular hide-accessible'>No Data</dt>
 				<dd className='legend__def fill-none' />
 			</dl>
 		</li>
 		<li className='legend__item'>
-			<p className='legend__subhed'>Tie</p>
+			<p className='legend__subhed benton-regular'>Tie</p>
 			<dl className='legend__deflist'>
-				<dt className='legend__term visually-hidden'>Tie</dt>
+				<dt className='legend__term benton-regular hide-accessible'>Tie</dt>
 				<dd className='legend__def fill-tie' />
 			</dl>
 		</li>
 		<li className='legend__item'>
-			<p className='legend__subhed'>Lead</p>
+			<p className='legend__subhed benton-regular'>Lead</p>
 			<dl className='legend__deflist'>
 				{parties.map(party => (
-					[<dt className='legend__term'>{standardizeParty(party)}</dt>,
-						<dd className={`legend__def hash-${party}`} />]
+					[<dt className='legend__term benton-regular'>{standardizeParty(party)}</dt>,
+						<dd className={`legend__def fill-leading-${party}`} />]
 				))}
 			</dl>
 		</li>
 		<li className='legend__item'>
-			<p className='legend__subhed'>Win</p>
+			<p className='legend__subhed benton-regular'>Win</p>
 			<dl className='legend__deflist'>
 				{parties.map(party => (
-					[<dt className='legend__term'>{standardizeParty(party)}</dt>,
-						<dd className={`legend__def fill-${party}`} />]
+					[<dt className='legend__term benton-regular'>{standardizeParty(party)}</dt>,
+						<dd className={`legend__def fill-complete-${party}`} />]
 				))}
 			</dl>
 		</li>
