36ca52ea7a7deb8e9d1fdd310eb85c957e914db9
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-26 19:21:10 -0400

    Add visUtils.buildSeatColumns, the transposition of buildSeatRows

--- common/utils/visUtils.js
+++ common/utils/visUtils.js
@@ -4,7 +4,43 @@ import _ from 'lodash'
 import { transpose } from 'd3-array'
 
 /**
- * Builds a seating chart for balance of power charts.
+ * Builds a seating chart for balance of power charts. Returns the columns.
+ * @memberof visUtils
+ * @function
+ * @param {number} $0.dem the number of democratic seats
+ * @param {number} $0.ind the number of independent seats
+ * @param {number} $0.gop the number of gop seats
+ * @param {number} $0.total the total number of seats
+ * @param {number} $0.rows the desired number of rows
+ * @returns {Array} an array of columns of seats
+ */
+const buildSeatColumns = ({ dem = 0, gop = 0, ind = 0, total = 0,
+rows = 0 }) => {
+
+	const undecided = total - (dem + gop + ind)
+
+	// Create an array of dem, ind, undecided, and gop, in that order.
+	const seats = [].concat(
+		_.range(dem).map(() => 'dem'),
+		_.range(ind).map(() => 'ind'),
+		_.range(undecided).map(() => 'undecided'),
+		_.range(gop).map(() => 'gop'),
+	)
+
+	// Partition seats into chunks of size `rows`, thus creating a matrix.
+	const matrix = _.chunk(seats, rows)
+
+	// Add party and seat to every matrix element.
+	const result = matrix.map(row =>
+		row.map((party, i) => ({ party, seat: i + 1 }))
+	)
+
+	return result
+
+}
+
+/**
+ * Builds a seating chart for balance of power charts. Returns the rows.
  * @memberof visUtils
  * @function
  * @param {number} $0.dem the number of democratic seats
@@ -45,4 +81,5 @@ rows = 0 }) => {
 export {
 	// eslint-disable-next-line import/prefer-default-export
 	buildSeatRows,
+	buildSeatColumns,
 }
