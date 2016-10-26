059d392dc2191cd5184b37bd596f3a66a40cdd6b
Gabriel Florit <gabrielflorit@gmail.com>
2016-10-26 01:31:37 -0400

    Refactor

--- common/containers/PresidentMA.js
+++ common/containers/PresidentMA.js
@@ -7,12 +7,14 @@ import Footer from './../components/Footer.js'
 import Timer from './../components/Timer.js'
 import TownResultsTable from './../components/TownResultsTable.js'
 import Map from './../components/Map.js'
-import TOWNS from './../../data/output/TOWNS.json'
 import TestStatus from './../components/TestStatus.js'
 import ElectoralCollegeBar from './../components/ElectoralCollegeBar.js'
 import Hero from './../components/Hero.js'
 import LinkButton from './../components/LinkButton.js'
 import urlManager from './../utils/urlManager.js'
+
+import getTownsShapefile from './../utils/getTownsShapefile.js'
+const TOWNS = getTownsShapefile()
 
 import {
 	sortByVoteCount,
