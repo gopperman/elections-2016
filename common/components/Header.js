e7fc7659b7c912e4d04a795ca0b1e3d111a49348
elaina <elaina.natario@gmail.com>
2016-10-28 12:12:52 -0400

    header globe logo

--- common/components/Header.js
+++ common/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import svgs from './../utils/svgs.js'
 import Navigation from './Navigation.js'
 
 const Header = () => (
 	<header className='header' key='header'>
 		<a href='#content' className='skip-to-main'>Skip to main content</a>
+		<div className='header__logo'>
+			<a		
+					href='http://www.bostonglobe.com/'
+					className='header__logo-link'
+					dangerouslySetInnerHTML={{ __html: svgs.globeLogo }} />
+		</div>
 		<Navigation />
 	</header>
 )
