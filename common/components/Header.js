bcb15e61bd825c6b7604f7a68ff9c7b17c6567c5
elaina <elaina.natario@gmail.com>
2016-10-28 11:01:09 -0400

    add skip to main for accessibility

--- common/components/Header.js
+++ common/components/Header.js
@@ -3,6 +3,7 @@ import Navigation from './Navigation.js'
 
 const Header = () => (
 	<header className='header' key='header'>
+		<a href='#content' className='skip-to-main'>Skip to main content</a>
 		<Navigation />
 	</header>
 )
