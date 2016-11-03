84b20f2e4422b6ce041143da845127fbf7dbff5f
Gabriel Florit <gabrielflorit@gmail.com>
2016-11-03 12:27:19 -0400

    Remove specific ids from homepageEntry

--- client/homepageEntry.js
+++ client/homepageEntry.js
@@ -2,7 +2,11 @@
 
 (function outerClosure() {
 
-	globe.onDefine('window.jQuery && $("#elections-HpElectoralCollege").length', () => {
+	const name = process.env.HP_CONTAINER
+
+	const selector = `window.jQuery && $("#elections-${name}").length`
+
+	globe.onDefine(selector, () => {
 
 		require('es6-promise').polyfill()
 		require('./homepageIndex.js')
