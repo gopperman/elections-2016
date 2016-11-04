c5771bdb11c99051f5ca2ba195722cb7268f040d
Greg Opperman <gopperman@gmail.com>
2016-11-04 19:11:24 -0400

    Markup for breaking bar, minus svg dependent thing

--- common/components/BreakingBar.js
+++ common/components/BreakingBar.js
@@ -5,13 +5,29 @@ const BreakingBar = (alert) => {
 	const time = formatTime(new Date(alert.timestamp))
 
 	return (
-		<div className='r-block'>
-			<p><span>Breaking:</span> <a href={alert.link}>{time} {alert.headline}</a></p>
-			Tweet this update
-			<span className='close'>X</span>
+		<div className='breaking'  aria-live='polite'>
+			<div className='breaking__content'>
+				<p className='breaking__copy benton-bold'>
+					<mark>Breaking:</mark> <a href={alert.link} target='_blank' >{alert.headline}</a>
+					<time datetime={alert.timestamp} className='benton-regular'>{time}</time>
+				</p>
+			</div>
 		</div>
 	)
 }
+
+/* 
+				<a 
+					className='btn--tw' 
+					href='"https://twitter.com/intent/tweet?text=Breaking%3A%20Giant%20meteor%20hurdles%20toward%20Earth&via=BostonGlobe&url=https://bostonglobe.com/elections/2016'
+					target='_blank'
+					dangerouslySetInnerHTML={{ __html: svgs.twitterSvg }} />
+
+			<button
+				className='btn--close'
+				onClick={this.clearTooltip}
+				dangerouslySetInnerHTML={{ __html: svgs.closeSvg }} />
+*/
 
 BreakingBar.propTypes = {
 	item: PropTypes.object.isRequired,
