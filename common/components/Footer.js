3d897bdf6cdc0292e13aac937f2fd9e4790a68e0
elaina <elaina.natario@gmail.com>
2016-11-05 18:14:14 -0400

    fix #445

--- common/components/Footer.js
+++ common/components/Footer.js
@@ -4,6 +4,7 @@ import RaceNavigationLinks from './RaceNavigationLinks.js'
 const Footer = () => (
 	<footer className='footer' key='footer' role='contentinfo'>
 		<RaceNavigationLinks isFooter />
+		<p className='footer__source benton-regular'>Data source: AP</p>
 	</footer>
 )
 
