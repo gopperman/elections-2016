024100d5bd5bd9643c92e31a57d97c5343729a2d
Gabriel Florit <gabrielflorit@gmail.com>
2016-11-04 21:20:24 -0400

    Lint

--- common/components/BreakingBar.js
+++ common/components/BreakingBar.js
@@ -1,3 +1,4 @@
+import classnames from 'classnames'
 import React, { Component, PropTypes } from 'react'
 import { formatTime } from './../utils/timeUtil.js'
 import svgs from './../utils/svgs.js'
@@ -14,34 +15,46 @@ class BreakingBar extends Component {
 	}
 
 	render() {
-		const {isVisible} = this.state
-		const {alert} = this.props
+
+		const { isVisible } = this.state
+		const { alert } = this.props
 
 		const time = formatTime(new Date(alert.timestamp))
 
 		const encodedURI = encodeURIComponent(alert.link)
 		const encodedHeadline = encodeURIComponent(alert.headline)
 
-		const breakingClass = `breaking${(isVisible) ? '' : ' is-hidden'}`
+		const breakingClass =
+			classnames('breaking', { 'is-hidden': !isVisible })
+
+		// eslint-disable-next-line max-len
+		const twitterHref = `https://twitter.com/intent/tweet?text=${encodedHeadline}&via=BostonGlobe&url=${encodedURI}`
 
 		return (Object.keys(alert).length) ? (
-			<div className={breakingClass}  aria-live='polite'>
+			<div className={breakingClass} aria-live='polite'>
 				<div className='breaking__content'>
 					<p className='breaking__copy benton-bold'>
-						<mark>Breaking:</mark> <a href={alert.link} target='_blank' >{alert.headline}</a>
-						<time dateTime={alert.timestamp} className='benton-regular'>{time}</time>
+						<mark>Breaking:</mark>
+						<a
+							href={alert.link}
+							rel='noopener noreferrer'
+							target='_blank'>{alert.headline}</a>
+						<time
+							dateTime={alert.timestamp}
+							className='benton-regular'>{time}</time>
 					</p>
-					<a 
-						className='btn--tw' 
-						href={`https://twitter.com/intent/tweet?text=${encodedHeadline}&via=BostonGlobe&url=${encodedURI}`}
+					<a
+						className='btn--tw'
+						href={twitterHref}
 						target='_blank'
+						rel='noopener noreferrer'
 						dangerouslySetInnerHTML={{ __html: svgs.twitterSvg }} />
 				</div>
 				<button
 					className='btn--close'
 					onClick={this.clearTooltip}
 					dangerouslySetInnerHTML={{ __html: svgs.closeSvg }} />
-				</div>
+			</div>
 		) : null
 	}
 }
