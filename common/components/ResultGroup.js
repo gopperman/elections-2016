bad48d37a4a619b27a90e336da70e4787c1eebf6
Gabriel Florit <gabrielflorit@gmail.com>
2016-11-05 17:52:24 -0400

    Put em spacing inside overline

--- common/components/ResultGroup.js
+++ common/components/ResultGroup.js
@@ -20,11 +20,11 @@ numWinners, hideCheckmark }) => {
 
 		const suffix = overlineSuffix ?
 			(<span className='overline--meta benton-regular'>
-				{overlineSuffix} results
+				&#8195;{overlineSuffix} results
 			</span>) : null
 
 		return overline ?
-			<h3 className={klass}>{overline}&#8195;{suffix}</h3> : null
+			<h3 className={klass}>{overline}{suffix}</h3> : null
 
 	}
 
