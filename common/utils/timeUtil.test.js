a3306831829ba862b08ea4cc83c0b2f47b2a284d
Gabriel Florit <gabrielflorit@gmail.com>
2016-11-04 21:21:34 -0400

    Lint

--- common/utils/timeUtil.test.js
+++ common/utils/timeUtil.test.js
@@ -1,23 +1,29 @@
+/* global describe, it, afterEach */
+
 import assert from 'assert'
 import { formatTime } from './timeUtil.js'
 
 describe('timeUtil', () => {
 
 	describe('formatTime', () => {
+
 		it('Should return difference in minutes if post is less than an hour old', () => {
 			const now = formatTime(new Date())
 
 			assert.equal(now, '0 minutes ago')
 		})
+
 		it('Should return one minute, not one minutes, if post is one minute old', () => {
-			let time = new Date()
+			const time = new Date()
 			time.setMinutes(time.getMinutes() - 1)
 			assert.equal(formatTime(time), '1 minute ago')
 		})
+
 		it('Should return time in hh:mm format if post is more than an hour old', () => {
 			const moonWalk = formatTime(new Date(-14159040000))
 
 			assert.equal(moonWalk, '10:56 p.m.')
 		})
+
 	})
 })
