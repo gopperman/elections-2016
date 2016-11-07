04dd426bc8c5423c18b310fc8838b31b2444a739
elaina <elaina.natario@gmail.com>
2016-11-07 15:04:22 -0500

    fix #487

--- common/components/Promo.js
+++ common/components/Promo.js
@@ -6,7 +6,7 @@ const Promo = () => (
 		<div className='promo__content'>
 			<p className='benton-bold promo__hed'>Breaking news in your inbox</p>
 			<p className='benton-regular promo__copy'>Find out about important stories as they break with our <em className='benton-bold'>free</em> newsletter.</p>
-			<a className='benton-bold btn--feature' href='#' target='_blank'>Sign up now</a>
+			<a className='benton-bold btn--feature' href='http://pages.email.bostonglobe.com/BreakingNewsSignUp/' target='_blank'>Sign up now</a>
 		</div>
 	</div>
 )
